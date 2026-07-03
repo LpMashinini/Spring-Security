35f7f7ac1211f012ae064d2b49fcfe8ca7c93435
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-03 02:22:50 +0200

    add entities

--- src/main/java/com/example/security/domain/entity/UserEntity.java
+++ src/main/java/com/example/security/domain/entity/UserEntity.java
@@ -1,10 +1,21 @@
 package com.example.security.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class UserEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
 }
