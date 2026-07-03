bc23d7745616884eb2c09856da922634b90fbce5
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-03 02:34:32 +0200

    add UserRepository

---
+++ src/main/java/com/example/security/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.security.repository;
+
+import com.example.security.domain.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsernameAndIsActive(String username);
+}
