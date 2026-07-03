fd0972b16202452f81646389f9867bcbbfedf7b3
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-03 22:39:03 +0200

    add dao authentication provideder manager

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -3,6 +3,9 @@ package com.example.security.config;
 import com.example.security.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,5 +31,12 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService(){
         return new UserService();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService){
+
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(userDetailsService);
+        return new ProviderManager(daoAuthenticationProvider);
     }
 }
