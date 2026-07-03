0d3a3eaddef6998318a9c3981acb1a63d4c40eb3
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-03 02:13:46 +0200

    database connection

---
+++ src/main/java/com/example/security/domain/entity/UserEntity.java
@@ -0,0 +1,10 @@
+package com.example.security.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+}
