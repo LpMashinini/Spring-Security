311c2451c9b446266309ab0b289f12fee368db34
LpMashinini <lebogangmatshinini@gmail.com>
2026-07-04 15:01:41 +0200

    add jwt generate method

---
+++ src/main/java/com/example/security/service/JwtService.java
@@ -0,0 +1,29 @@
+package com.example.security.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+
+@Service
+public class JwtService {
+
+    public static final String SECRETS = "c7f4a9d2e1b8f6a3c5d7e9f0a1b2c3d4e5f6a7b8c9d0e1f2a3b4c5d6e7f8a9b0c1d2e3f4a5b6c7d8e9f0a1b2c3d4e5f6";
+    public String generateToken(String username){
+       return Jwts.builder().
+                setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30))
+                .addClaims(new HashMap<>())
+                .signWith(getSignedKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSignedKey(){
+        return Keys.hmacShaKeyFor(SECRETS.getBytes());
+    }
+}
